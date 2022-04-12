21a96e3715ef48b1adf2fca90c8ff81e53936973
nperez97 <66038086+nperez97@users.noreply.github.com>
2022-04-11 21:46:27 -0600

    fixed navbar

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles';
 import jesus from './Product/Images/Jesus.png';
 
 const products = [
@@ -10,8 +11,11 @@ const products = [
 ];
 
 const Products = () => {
+    const classes = useStyles();
+
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
                 {products.map(product => (
                     <Grid item xs={12} sm={6} md={4} key={product.id}>
